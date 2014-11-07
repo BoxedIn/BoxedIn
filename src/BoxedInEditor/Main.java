63724eab5582bdb77bc99f0a7b3a6cb2dce3e781
diko5246 <ahankewycz@gmail.com>
2014-11-07 17:55:04 -0500

    Create Main.java

---
+++ src/BoxedInEditor/Main.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BoxedInEditor;
+
+/**
+ *
+ * @author azh5442
+ */
+public class Main {
+    public static void main(String args[]){
+        GameComposer gc = new GameComposer();
+    }
+}
