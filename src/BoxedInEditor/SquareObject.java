f9ed16f13fa5f06a6efa813fddcc19a2d94c08ec
diko5246 <ahankewycz@gmail.com>
2014-11-07 17:53:48 -0500

    Create SquareObject.java

---
+++ src/BoxedInEditor/SquareObject.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BoxedInEditor;
+
+import java.awt.Image;
+import java.awt.Point;
+
+/**
+ *
+ * @author azh5442
+ */
+public class SquareObject extends GameObject{
+        public static Image gOImage;
+
+        public SquareObject(Point p){
+            location = p;
+        }
+        public SquareObject(SaveableGameObject sgo){
+            this.location = sgo.p;
+            this.setHeight(sgo.h);
+            this.setWidth(sgo.w);
+        }
+        public void draw(){
+            int x = this.location.x*Level.boxPixelWidth;
+            int y = this.location.y*Level.boxPixelHeight;
+            levelGraphics.drawImage(gOImage, x, y, null);
+            levelGraphics.drawLine(0, 0, 50,  50);
+        }    
+}
