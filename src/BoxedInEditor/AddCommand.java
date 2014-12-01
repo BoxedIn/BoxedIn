a091cb6505b25b1ba851e8e707c0fce3020638c4
jsteen22 <jsteen6412@gmail.com>
2014-12-01 15:47:57 -0500

    Update AddCommand.java

--- src/BoxedInEditor/AddCommand.java
+++ src/BoxedInEditor/AddCommand.java
@@ -1,9 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package BoxedInEditor;
 
 import java.awt.Point;
@@ -14,28 +12,43 @@ import java.awt.Point;
  */
 public class AddCommand extends Command{
     private Level level;
-    private Point p;
-    private int object;
-    public AddCommand(int type, Point p){
-        object = type;
-        this.p = p;
-    }
-    public void doCom(){
-        GameObject o = null;
-        switch (object){
-            case 1: o = new SquareObject(p);
+    private GameObject go;
+    
+    public AddCommand(Level l, int type, Point p){
+        level = l;
+        go = null;
+        
+        switch (type){
+            case 1: go = new SquareObject(p);
                     break;
-            case 2: o = new TriangleObject(p);
+            case 2: go = new TriangleObject(p);
                     break;
-            case 3: o = new CircleObject(p);
+            case 3: go = new CircleObject(p);
+                    break;
+            case 4: go = new StartPoint(p);
+                    break;
+            case 5: go = new EndPoint (p);
                     break;
             default:
                     break;
         }
-        if(o != null){
-            level.addGameObject(o);}
     }
-    public void undoCom(){
-        level.removeGameObject(p); 
+    
+    public boolean doCom(){
+        boolean success = false;
+        if(go != null){
+            if(level.addGameObject(go)){
+                success = true;
+            }
+        }
+        return success;
+    }
+    
+    public boolean undoCom(){
+        boolean success = false;
+        if(level.removeGameObject(go.getLocation())!=null){
+            success = true;
+        } 
+        return success;
     }
 }
