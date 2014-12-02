6e2d59472d229833dbbbb433bdad2f1d8dc67dd1
diko5246 <ahankewycz@gmail.com>
2014-12-02 17:08:55 -0500

    Create MoveableObject.java

---
+++ src/BoxedInEditor/MoveableObject.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package BoxedInEditor;
+
+/**
+ *
+ * @author jds5782
+ */
+public abstract class MoveableObject extends GameObject{
+    
+}
