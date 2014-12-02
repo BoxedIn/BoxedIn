6dd0a27fc36cbd6ac9b70263cee8f8d14ca7f266
diko5246 <ahankewycz@gmail.com>
2014-12-02 17:12:54 -0500

    Create UninitializedGraphicsException.java

---
+++ src/BoxedInEditor/UninitializedGraphicsException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BoxedInEditor;
+
+/**
+ *
+ * @author azh5442
+ */
+public class UninitializedGraphicsException extends Exception{
+    
+    public UninitializedGraphicsException(String message){
+        super(message);
+    }
+}
