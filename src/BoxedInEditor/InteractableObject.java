0af1e9a4a2387c504fca9a5f9b7d787232ab763a
Andrew Hankewycz <ahankewycz@gmail.com>
2015-01-20 18:58:54 -0500

    Create InteractableObject.java

---
+++ src/BoxedInEditor/InteractableObject.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BoxedInEditor;
+
+/**
+ *
+ * @author azh5442
+ */
+public interface InteractableObject{
+   //needed to make objects interactable 
+   //currently only teleport pad is but additional interactable objects could
+   //could be implemented in the future
+}
