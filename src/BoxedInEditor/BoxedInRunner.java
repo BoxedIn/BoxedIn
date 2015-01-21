42170c8eaf7ddcc2a415459b7a0a1a202d796302
Andrew Hankewycz <ahankewycz@gmail.com>
2015-01-20 19:07:53 -0500

    Update BoxedInRunner.java

--- src/BoxedInEditor/BoxedInRunner.java
+++ src/BoxedInEditor/BoxedInRunner.java
@@ -7,57 +7,135 @@
 package BoxedInRunner;
 
 import BoxedInEditor.EndPoint;
-import BoxedInEditor.Game;
 import BoxedInEditor.GameObject;
+import BoxedInEditor.InteractableObject;
 import BoxedInEditor.Level;
 import BoxedInEditor.LevelManager;
 import BoxedInEditor.MoveableObject;
 import BoxedInEditor.NonmoveableObject;
-import BoxedInEditor.Player;
 import java.awt.Point;
-import java.io.EOFException;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.ObjectInputStream;
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author tmb5577
  */
-public class BoxedInRunner extends LevelManager{
-    BoxedInRunnerUI birui = new BoxedInRunnerUI(this);
-    Game g = new Game();
-    int levelNumber;
+public class BoxedInRunner extends LevelManager implements TimerObserver{
+    private BoxedInRunnerUI birui = new BoxedInRunnerUI(this);
+    private Timer timer;
+    private Thread timerThread;
+    private boolean paused = true; 
     
     public BoxedInRunner(){
         birui.setVisible(true);
-        try {
-            this.loadGame();
-            birui.refreshVariables();
-            birui.repaint();
-        } catch (IOException ex) {
-            System.err.println(ex);
-        }
+        welcomeMessage();
+        birui.repaint();
+        timer = Timer.getInstance();
+        timerThread = new Thread(timer);
+        timerThread.start();
+        timer.subscribe(birui);
+        timer.subscribe(this);
+        openGame();
+        birui.refreshVariables();
     }
     
     public void movePlayer(int d){
-        currentLevel.movePlayer(d);
+        getCurrentLevel().movePlayer(d);
+        
+        if((int)getCurrentLevel().getPlayer().getLocation().getX() == (int)getCurrentLevel().getEnd().getX() && 
+           (int)getCurrentLevel().getPlayer().getLocation().getY() == (int)getCurrentLevel().getEnd().getY()){
+            System.out.printf("End point (%d, %d)\n", (int)getCurrentLevel().getEnd().getX(), (int)getCurrentLevel().getEnd().getY());
+            System.out.printf("Start point (%d, %d)\n", (int)getCurrentLevel().getPlayer().getX(), (int)getCurrentLevel().getPlayer().getY());
+            nextLevel();
+        }
+    }
+    
+    private void nextLevel(){
+        setLevelNumber((getLevelNumber()+ 1));
+        if(getGame().getLevel(getLevelNumber()) != null){
+            setCurrentLevel(getGame().getLevel(getLevelNumber()));
+            levelIsNotSaved();      // we've moved to the next level, mark that the game hasnt been saved yet
+            timer.setTimer(getCurrentLevel().getTimeLimit());
+            timer.startTimer();
+            System.out.println(getCurrentLevel().getLevelName());
+            System.out.printf("(%d, %d)\n", (int)getCurrentLevel().getEnd().getX(), (int)getCurrentLevel().getEnd().getY());
+            System.out.println("Time for the next level. Hope you're ready!");
+        }else{
+            System.out.println("You've won the game Congrats!");
+            win();
+        }
     }
     
     public boolean levelIsOpen(){
-        if(currentLevel != null){
+        if(getCurrentLevel() != null){
             return true;
         }else{
             return false;
         }
     }
+   
+    @Override
+    public void updateTime(int seconds){
+        if(seconds <= 0){
+            lose();
+        }
+    }
+    
+    public void pauseGame(){
+        paused = true;  // enable pause
+        timer.stopTimer();
+    }
+    
+    public void resumeGame(){
+        paused = false; // resume game
+        timer.startTimer();
+    }
+    
+    public boolean isPaused(){
+        return paused;
+    }
+    
+    public void win(){
+        pauseGame();      // pause the game so keyboard inputs dont throw exceptions
+        setCurrentLevel(null);    // stops keyboard from taking inputs
+        birui.disableSaveButton();
+        levelIsSaved();    // set it to saved so that it doesnt prompt to save on close
+        displayGameWin();
+    }
+    
+    public void lose(){
+        pauseGame();      // pause the game so keyboard inputs dont throw exceptions
+        displayGameLose();
+    }
+    
+    public void restartLevel(){
+        if(getCurrentLevel() != null){
+            try{
+                loadGame(true);         // true is a flag for restarting the level
+                timer.setTimer(getCurrentLevel().getTimeLimit());
+                timer.startTimer();
+                birui.refreshVariables();
+                resumeGame();
+            }catch(ClassCastException ex){
+                System.err.println("incompatable filetype selected");
+                JOptionPane.showMessageDialog(birui, "Incompatible filetype was selected");
+            }
+        }
+    }
+    
+    private void displayGameWin(){
+        JOptionPane.showMessageDialog(birui, "Congratulations! You've won the game!");
+    }
+    
+    private void displayGameLose(){
+        JOptionPane.showMessageDialog(birui, "Nice Try\nBut you'll have to be quicker than that");
+    }
     
     public boolean processMove(int move){
-        
-        Point playerPosition = currentLevel.getPlayer().getLocation();//space occupied by character
+        // returns true if the player is allowed to move, false otherwise
+        Point playerPosition = getCurrentLevel().getPlayer().getLocation();//space occupied by character
         Point nextPosition = new Point();//space character wants to move to
         Point nextNextPosition = new Point();//space after space character wants to move to
         boolean okayToMove;     // will be set by collionDetection method
@@ -88,21 +166,15 @@ public class BoxedInRunner extends LevelManager{
             nextNextPosition.y = playerPosition.y;
         }
         
+        //ensure player does not move out of bounds
+        if(nextPosition.y<0 || nextPosition.y>19 || nextPosition.x<0 || nextPosition.x>19){
+                    System.out.println("NOOO");
+                    return okayToMove = false;
+        }
+        
         //collision detection
         okayToMove = collisionDetection(playerPosition, nextPosition, nextNextPosition);
-        
-        if(currentLevel.getPlayer().getLocation() == currentLevel.getEnd().getLocation()){
-//            birui.displayWin();
-            levelNumber++;
-            currentLevel = g.getLevel(levelNumber);
-            if(currentLevel == null){
-                //this.win();
-                System.out.println("You've won the game Congrats!");
-            }
-            else{
-                System.out.println("Time for the next level. Hope you're ready!");
-            }
-        }
+      
         return okayToMove;
     }
     
@@ -110,96 +182,66 @@ public class BoxedInRunner extends LevelManager{
     public boolean collisionDetection(Point current, Point next, Point nextnext){
         
         boolean canmove = false;
-        GameObject go[][] = currentLevel.getGameObject();
+        GameObject go[][] = getCurrentLevel().getGameObject();
 
         //player trying to move to an open space
         if(go[current.x][current.y] instanceof MoveableObject && go[next.x][next.y] == null) {
-            System.out.println("Go ahead sir you can move to that empty space");
             canmove = true;
         }
         //player trying to move to the end point
         else if(go[current.x][current.y] instanceof MoveableObject && go[next.x][next.y] instanceof EndPoint) {
-            System.out.println("Go ahead sir you can move to the end point...");
-            System.out.println("...I wonder what will happen next...");
             canmove = true;
         }
-        //player trying to move a box
+        else if(go[current.x][current.y] instanceof MoveableObject && go[next.x][next.y] instanceof InteractableObject){
+            canmove = true;
+        }
+        //player trying to move a boulder/block
         else if(go[current.x][current.y] instanceof MoveableObject && go[next.x][next.y] instanceof NonmoveableObject) {
-            System.out.println("Fuck you dumbass thats a block you can't move there!");
             canmove = false;
         }
-        //player trying to move a boulder
+        //player trying to move a box
         else if(go[current.x][current.y] instanceof MoveableObject && go[next.x][next.y] instanceof MoveableObject) {
+            
+            //player trying to push box off screen
+            if(nextnext.y<0 || nextnext.y>19 || nextnext.x<0 || nextnext.x>19){
+                System.out.println("NOOO");
+                return canmove = false;
+            }
             
             //check to see if the space behind the boulder is open
             if(go[nextnext.x][nextnext.y] == null){
-                System.out.println("Go ahead sir you can move that boulder to that empty space");
                 canmove = true;
             }
             
             //if not theres a boulder or block behind it
             else{
-                System.out.println("Shit there seems to be a block and/or boulder in front of you and/or that boulder!");
-                System.out.println("Fuck you dumbass you can't move there");
                 canmove = false;
             }
         }
         return canmove;
     }
     
-    
-    // ************* josh, i made changes to this method to allow me to write some of the drawing
-    // for the display. please finalize this method as far as what you want to do with setting
-    // the currentLevel and stuff. also i dont think this should be throwing these exceptions. 
-    // each call to this method will have to have a try/catch block.... 
-    // maybe catch them inside the method and use a boolean return if you want some way of 
-    // checking if the game was successfully opened
-    // *******************
-    public void loadGame() throws FileNotFoundException, IOException{
-        Level l;
-        File fileName = selectFile();
-        Player p;
-        levelNumber = 0;
-        
-        fin = new FileInputStream(fileName);
-        ois = new ObjectInputStream(fin);
-        
-        int i = 0;
-        try{
-            while((l = readLevel(ois))!= null){
-                p = new Player(l.getStart().getLocation());
-                l.removeGameObject(l.getStart().getLocation());
-                l.addGameObject(p);
-                l.setPlayer(p);
-                g.addLevel(l, Integer.toString(i));
-                i++;
-            }
-        }catch(ClassNotFoundException ex){
-            System.err.println(ex);
-        }catch(EOFException ex){
-            System.out.println("reached end of file");
-        }
-        ois.close(); 
-        
-        Level temp = g.getLevel(0); // for testing, manually set the level
-        if(temp != null){
-            setLevel(temp);     // set the currentLevel in the levelmanager to the first level
-            System.out.println("current level has been set");
-        }else{
-            System.out.println("current level could not be set");
-        }
-    }
-    
     public void saveGame(){
-        
+        restartLevel();
+        super.setGame(this.getGame());
+        buildGame();
+        levelIsSaved();     // set the boolean variable so it doesnt prompt for saving on close
     }
-    public void continueGame(){
-        
-    }
-    public void pauseGame(){
-        
-    }
-    public void beginGame(){
+    public void openGame(){
+        getGame().setFilePath(null);
+        try{
+            loadGame(false);
+        }catch(ClassCastException ex){
+            System.err.println("incompatable filetype selected");
+            JOptionPane.showMessageDialog(birui, "Incompatible filetype was selected");
+        }
+        if(getCurrentLevel() != null){
+            timer.setTimer(getCurrentLevel().getTimeLimit());
+            timer.startTimer();
+            birui.refreshVariables();
+            resumeGame();
+            birui.enableSaveButton();
+        }
     }
        
     public File selectFile(){
@@ -209,12 +251,20 @@ public class BoxedInRunner extends LevelManager{
         return fileName;
     }
     
-    public void win(){
-        birui.displayGameWin();
-    }
-    public void restartLevel(){
-        this.currentLevel = g.getLevel(levelNumber);
-    }
+    private void welcomeMessage(){
+        String str3 = "When you close this box another will open with a file directory\n";
+        str3 += "Please select a \"game\" file you created in the editor to use, then click anywhere on the gray screen\n";
+        str3 += "Press the HELP button at any time for more info";
 
+        JOptionPane.showMessageDialog(birui, str3, "", JOptionPane.INFORMATION_MESSAGE, null);
 
+    }
+    
+    public void helpMessage(){
+        String str = "Help Menu\n";
+        str += "You can move boxes around the level, but not bricks or boulders\n";
+        str += "Try stepping on the small teleporter pad to see what happens\n";
+        str += "Press Restart Game at any time if you get stuck or Pause Game if you just need a break\n";
+        JOptionPane.showMessageDialog(birui, str);
+    }
 }
