a1df7af00ebb911172fe3882d398fdf9ef332497
Andrew Hankewycz <ahankewycz@gmail.com>
2015-01-20 19:09:44 -0500

    Create TimerObserver.java

---
+++ src/BoxedInEditor/TimerObserver.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BoxedInRunner;
+
+/**
+ *
+ * @author azh5442
+ */
+public interface TimerObserver {
+    public void updateTime(int seconds);
+}
