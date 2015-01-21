e71f462cac3434a0dbfc5b0abfbe362924ad3fcc
Andrew Hankewycz <ahankewycz@gmail.com>
2015-01-20 19:02:29 -0500

    Update MoveableObject.java

--- src/BoxedInEditor/MoveableObject.java
+++ src/BoxedInEditor/MoveableObject.java
@@ -6,10 +6,20 @@
 
 package BoxedInEditor;
 
+import java.awt.Point;
+
 /**
  *
  * @author jds5782
  */
 public abstract class MoveableObject extends GameObject{
+    public void setX(int x){
+        Point p = new Point(x, getLocation().y);
+        setLocation(p);
+    }
     
+    public void setY(int y){
+        Point p = new Point(getLocation().x, y);
+        setLocation(p);
+    }
 }
